a586115656bebf4674c0601f9313fdc6b9af36b7
Dr Serge Victor <ser@users.noreply.github.com>
2023-10-07 19:03:17 +0700

    oh!

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,16 @@
+return {
+
+{
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
+
+
+
+}
