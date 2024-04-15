6984f5ca6d64b37bc39ef1c9ec2371f2e863ccfb
Dr Serge Victor <ser@users.noreply.github.com>
2024-04-15 08:44:40 +0700

    new plugings

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        api_host_cmd = "echo -n https://ai-api.sergevictor.eu",
+        api_key_cmd = "echo pilomilo",
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+}
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,16 +1,40 @@
 return {
 
-{
-  "folke/tokyonight.nvim",
-  opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
-}
+  {
+    "eldritch-theme/eldritch.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
 
+  -- Configure LazyVim to load theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "eldritch",
+    },
+  },
 
-
+  -- https://github.com/loganswartz/sunburn.nvim
+  --  {
+  --    "loganswartz/sunburn.nvim",
+  --    dependencies = { "loganswartz/polychrome.nvim" },
+  --   -- you could do this, or use the standard vimscript `colorscheme sunburn`
+  --    config = function()
+  --      vim.cmd.colorscheme("sunburn")
+  --    end,
+  --  },
+  --
 }
